54d1b49efec68ff1e40080802c19de385f560976
Semih Basrık <basriksemih@gmail.com>
2025-04-26 20:09:24 +0300

    nft certificate dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Test from './components/Test'
 import Tutorial from './components/Tutorial'
 
+function createNFTCertificate(){
+}
+
 function App() {
+
+  const [isCertificateDialogOpen, setIsCertificateDialogOpen] = useState<boolean>(true);
 
   return (
     <div className='w-full flex flex-column'>
@@ -12,6 +18,14 @@ function App() {
       <div className='w-full flex flex-row fixed left-0 top-20 w-full'>
         <Tutorial/>
         <Test/>
+      </div>
+      <div>
+        <div className='mt-200'>
+          <button onClick={async () => {
+            await createNFTCertificate();
+            setIsCertificateDialogOpen(true);
+          }}>Get Certificate</button>
+        </div>
       </div>
     </div>
   )
