b4db57a285adcaf096f7c71d167072c9c2a790f5
Yesim Alırsatar <yesimalirsatar@hotmail.com>
2025-04-27 10:20:45 +0300

    değişiklik

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -4,7 +4,7 @@ export default function Test() {
     return (
         <div className="w-1/2">
             <div>
-                <p className="text-black-400 mt-10 text-4xl font-bold italic">Test✨</p>
+                <p className="text-black-400 mt-10 text-4xl font-bold">Test</p>
                 <SolanaQuiz/>
             </div>
         </div>
--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -4,7 +4,7 @@ export default function Tutorial() {
     return (
         <div className="flex justify-end">
             <div>
-                <p className="text-black-400 mt-10 text-4xl font-bold italic">Tutorial✨</p>
+                <p className="text-black-400 mt-10 text-4xl font-bold">Tutorial</p>
                 <SolanaTutorial></SolanaTutorial>
             </div>
         </div>
